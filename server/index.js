347d4f9608cb19f132cb7467b3c36361661c5f19
Ajay Yadav <128020496+ajay-rgb@users.noreply.github.com>
2025-08-12 19:21:13 +0000

    updated for deployment

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const port = 3000;
+const port = process.env.PORT || 3000; 
 
 app.use(cors());
 app.use(express.json()); 
